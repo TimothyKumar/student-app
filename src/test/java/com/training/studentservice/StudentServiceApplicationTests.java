6b8a56801e472b1e5df0b52173a32f6a1dd0b496
mahen <mahen518@gmail.com>
2023-09-20 22:10:39 +0530

    updated

--- src/test/java/com/training/studentservice/StudentServiceApplicationTests.java
+++ src/test/java/com/training/studentservice/StudentServiceApplicationTests.java
@@ -1,12 +1,9 @@
 package com.training.studentservice;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 class StudentServiceApplicationTests {
 
 	@Test
